f47eace4bca0f13c3d957e024882ea27a26376d6
Sébastien Lavoie <github@lavoie.sl>
2013-06-17 12:55:00 -0400

    added example front crontollers

---
+++ examples/app.php
@@ -0,0 +1,8 @@
+<?php
+
+use WMC\AppLoader\AppLoader;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+$app_loader = new AppLoader(__DIR__ . '/../app', $loader);
+$app_loader->run();
---
+++ examples/app_dev.php
@@ -0,0 +1,9 @@
+<?php
+
+use WMC\AppLoader\AppLoader;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+$app_loader = new AppLoader(__DIR__ . '/../app', $loader);
+$app_loader->environment = 'dev';
+$app_loader->run();
