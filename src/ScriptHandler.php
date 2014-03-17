99b9009df4b16ec74db0ab7e086c0781bede8a66
Sébastien Lavoie <github@lavoie.sl>
2014-03-17 11:04:08 -0400

    changed script to use WMC\Composer\Utils

---
+++ src/ScriptHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WMC\AppLoader;
+
+use Composer\IO\IOInterface;
+use Composer\Script\Event;
+use WMC\Composer\Utils\ConfigFile\YamlConfigFile;
+
+class ScriptHandler
+{
+    public static function buildParameters(Event $event)
+    {
+        $extras = $event->getComposer()->getPackage()->getExtra();
+
+        if (empty($extras['wmc-app-loader']['file'])) {
+            if (empty($extras['symfony-app-dir'])) {
+                throw new \InvalidArgumentException('Either extra.symfony-app-dir or extra.wmc-app-loader.file setting are required to use this script handler.');
+            }
+            $app_dir = $extras['symfony-app-dir'];
+            $app_loader = new AppLoader($app_dir, null);
+            $realFile = $app_loader->getDefaultOptionsFile();
+        } else {
+            $realFile = $extras['wmc-app-loader']['file'];
+        }
+
+        if (empty($extras['wmc-app-loader']['dist-file'])) {
+            $distFile = $realFile.'.dist';
+
+            if (!is_file($distFile)) {
+                // using packaged dist file
+                $distFile = __DIR__ . '/../../../app/config/app_loader.ini.dist';
+            }
+        } else {
+            $distFile = $extras['wmc-app-loader']['dist-file'];
+        }
+
+        $keepOutdatedParams = false;
+        if (isset($extras['wmc-app-loader']['keep-outdated'])) {
+            $keepOutdatedParams = (bool) $extras['wmc-app-loader']['keep-outdated'];
+        }
+
+        $yml = new YamlConfigFile($event->getIO());
+        $yml->setKeepOutdatedParams($keepOutdatedParams);
+        $yml->updateFile($realFile, $distFile);
+    }
+}
